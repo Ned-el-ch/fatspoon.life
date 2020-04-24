4523996e845e6551514b4e3ccece2c5344a8f548
Ned-el-ch <nedelchev.niki@gmail.com>
2020-04-24 19:55:46 +0100

    added link to recipe src in RecipeCard

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -8,6 +8,11 @@ const RecipeCard = ({recipe, starRecipe, unstarRecipe}) => {
 				<button className="rc-star-recipe-button" onClick={() => {starRecipe(recipe)}}>Star Recipe</button>
 				<button className="rc-unstar-recipe-button" onClick={() => {unstarRecipe(recipe.uri)}}>Unstar Recipe</button>
 			</div>
+			<div className="rc-source-container">
+				<a className="rc-source-link" href={recipe.url} target="_blank" rel="noopener noreferrer">
+					<span className="rc-source-link-text">Source: {recipe.source}</span>
+				</a>
+			</div>
 			<br/>
 		</div>
 	)
