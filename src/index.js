8ec2eedb58b19b0285aa554de167d3f35d4b37ff
Ned-el-ch <nedelchev.niki@gmail.com>
2020-04-25 19:31:28 +0100

    imported bootstrap.min.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './Styles/index.css';
 
 import store from './store'
