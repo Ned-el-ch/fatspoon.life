bdcf8cbdf72bb9dd597d2964907bac9d1a37ef5e
Ned-el-ch <nedelchev.niki@gmail.com>
2020-04-29 20:07:56 +0100

    labels and hidden buttons IngredientCard

--- src/components/IngredientCard.js
+++ src/components/IngredientCard.js
@@ -30,7 +30,7 @@ const IngredientCard = ({ingredient, increaseIngredient, decreaseIngredient, rem
 	const label = assignCategoryLabel(category);
 	return (
 		<div className={`ic-container ${label}`}>
-			<div className={`ic-heading-label ${label}`}>⬤</div>
+			<div className={`ic-heading-label ${label}`}></div>
 			<div className="ic-heading-container">
 				<span className={`ic-heading ${label}`}>{name}</span>
 			</div>
