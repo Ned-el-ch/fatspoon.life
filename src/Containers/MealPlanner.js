6ac7042e64aa8cd681da05564f1cc3ea5d72d607
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-01 08:44:42 +0100

    connect to store, add redirect if no recipes

--- src/Containers/MealPlanner.js
+++ src/Containers/MealPlanner.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import PageHeader from '../Components/PageHeader.js';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
 
-const MealPlanner = () => {
+const MealPlanner = ({ recipeCount, ingredientCount }) => {
 	return (
 		<div className="meal-planner-container">
 			<PageHeader title="My Meal Planner"/>
+			{recipeCount !== 0
+			?
+			<div>
+				
+			</div>
+			:
+			<div>
+				<Link to="/MyCookbook">Add some recipes first!</Link>
+			</div>
+			}
 		</div>
 	)
 }
 
-export default MealPlanner;
+const mapStateToProps = state => {
+	return (
+		{
+			recipeCount: state.recipes.length,
+			ingredientCount: state.ingredients.length
+		}
+	)
+}
+
+export default compose(
+	withRouter,
+	connect(mapStateToProps)
+)(MealPlanner);
