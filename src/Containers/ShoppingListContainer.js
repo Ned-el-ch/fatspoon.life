58a9c5350ff7a923d682ffbba7653c3348f471c2
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-01 09:42:27 +0100

    Update ShoppingListContainer.js

--- src/Containers/ShoppingListContainer.js
+++ src/Containers/ShoppingListContainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import ShoppingList from '../Components/ShoppingList'
 
 export const ShoppingListContainer = () => {
 	return (
 		<div className="shopping-list-container">
-			
+			<ShoppingList />
 		</div>
 	)
 }
