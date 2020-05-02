9c0149a072e7572dd2b184a1c76fcbb33a54318c
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-02 10:59:53 +0100

    add Title input group

--- src/Components/RecipeForm.js
+++ src/Components/RecipeForm.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import IngredientSelect from './IngredientSelect'
+import { InputGroup, FormControl, Row, Col } from "react-bootstrap";
 
 const RecipeForm = () => {
 	return (
 		<div className="recipe-form">
-			new recipe form goes here
-			<IngredientSelect />
+			<Row className="align-self-start justify-content-right">
+			<Col xs sm md lg xl={8} className="col-xxl">
+				<InputGroup className="mb-3">
+					<InputGroup.Prepend>
+						<InputGroup.Text id="inputGroup-sizing-lg">Title</InputGroup.Text>
+					</InputGroup.Prepend>
+					<FormControl
+						placeholder="Get Creative!"
+						aria-label="title"
+						aria-describedby="inputGroup-sizing-lg"
+					/>
+				</InputGroup>
+
+			</Col>
+			</Row>
+
+			{/* <IngredientSelect /> */}
 		</div>
 	)
 }
