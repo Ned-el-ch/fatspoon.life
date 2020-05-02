9e623c1a8092541eeba8bb5adc7bc3d06756034a
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-02 12:32:05 +0100

    Update RecipeForm.js

--- src/Components/RecipeForm.js
+++ src/Components/RecipeForm.js
@@ -5,8 +5,20 @@ import { InputGroup, FormControl, Row, Col } from "react-bootstrap";
 const RecipeForm = () => {
 	return (
 		<div className="recipe-form">
-			<Row className="align-self-start justify-content-center">
-			<Col xs sm md={{ span: 8, offset: 4 }}>
+			<Row>
+			<Col xs={12} sm={12} md={4} lg={{ span: 4}}>
+				<InputGroup className="mb-3">
+					<InputGroup.Prepend>
+						<InputGroup.Text id="inputGroup-sizing-lg"><span role="img" aria-label="emoji">🖼️</span></InputGroup.Text>
+					</InputGroup.Prepend>
+					<FormControl
+						placeholder="Recipe image URL goes here"
+						aria-label="main image"
+						aria-describedby="inputGroup-sizing-lg"
+					/>
+				</InputGroup>
+			</Col>
+			<Col xs={12} sm={12} md={8} lg={{ span: 8}}>
 				<InputGroup className="mb-3">
 					<InputGroup.Prepend>
 						<InputGroup.Text id="inputGroup-sizing-lg">Title</InputGroup.Text>
@@ -22,10 +34,10 @@ const RecipeForm = () => {
 			<Row className="align-self-start justify-content-center">
 			<Col xs sm md={{ span: 8, offset: 4 }}>
 				<InputGroup>
-					<InputGroup.Prepend>
+					<FormControl as="textarea" aria-label="Description" maxLength={220} style={{height: "140px", maxHeight: "140px", minHeight: "140px", resize: "none"}}/>
+					<InputGroup.Append>
 						<InputGroup.Text>Description</InputGroup.Text>
-					</InputGroup.Prepend>
-					<FormControl as="textarea" aria-label="Description" />
+					</InputGroup.Append>
 				</InputGroup>
 			</Col>
 			</Row>
