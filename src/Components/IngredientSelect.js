82e0efdc928e01c5a179a74bd4ead901c9e7bdcd
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-03 10:45:08 +0100

    Update IngredientSelect.js

--- src/Components/IngredientSelect.js
+++ src/Components/IngredientSelect.js
@@ -17,6 +17,33 @@ const CustomOption = (props) => {
 	);
 }
 
+const colors = {
+	"primary": "#ff6347",
+	"primary75": "#4C9AFF",
+	"primary50": "#B2D4FF",
+	"primary25": "#DEEBFF",
+	"danger": "#DE350B",
+	"dangerLight": "#FFBDAD",
+	"neutral0": "hsl(0, 0%, 100%)",
+	"neutral5": "hsl(0, 0%, 95%)",
+	"neutral10": "hsl(0, 0%, 90%)",
+	"neutral20": "hsl(0, 0%, 80%)",
+	"neutral30": "hsl(0, 0%, 70%)",
+	"neutral40": "hsl(0, 0%, 60%)",
+	"neutral50": "hsl(0, 0%, 50%)",
+	"neutral60": "hsl(0, 0%, 40%)",
+	"neutral70": "hsl(0, 0%, 30%)",
+	"neutral80": "hsl(0, 0%, 20%)",
+	"neutral90": "hsl(0, 0%, 10%)"
+}
+const theme = (theme) => ({
+...theme,
+borderRadius: "0.25rem",
+colors: {
+...colors
+},
+})
+
 const IngredientSelect = ({ingredients, defaultOptionIndex}) => {
 	const initialSelectedOption = {toAdd: null};
 	const [selectedOption, setSelectedOption] = useState(initialSelectedOption);
@@ -31,6 +58,7 @@ const IngredientSelect = ({ingredients, defaultOptionIndex}) => {
 			components={{Option: CustomOption}}
 			onChange={toAdd => setSelectedOption({toAdd})}
 			value={selectedOption.toAdd}
+			theme={theme}
 			placeholder="Add a new ingredient!"
 			noResultsText="Looks like I forgot to add this ingredient"
 		/>
