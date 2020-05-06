83da87683db1a76f36a9e5afd4c4135ffbf6d1e8
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 08:24:24 +0100

    add LOGOUT_USER reducer

--- src/Reducers/user.js
+++ src/Reducers/user.js
@@ -7,7 +7,8 @@ export default (state = initialState, action) => {
 	switch (action.type) {
 		case "LOGIN_USER":
 			return {...state, user: action.userData};
-
+		case "LOGOUT_USER":
+			return {...state, user: {}}
 		default:
 			return state;
 	}
