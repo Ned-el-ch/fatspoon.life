29d9585311bca898b8650bc719a9aa8185cbdefd
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 11:54:59 +0100

    Convert to functional component

--- src/Containers/NavbarContainer.js
+++ src/Containers/NavbarContainer.js
@@ -14,81 +14,50 @@ import { addRecipeResults } from "../Actions/search";
 import { compose } from 'redux'
 import { withRouter } from "react-router-dom";
 
-class NavbarContainer extends Component {
+const NavbarContainer = () => {
 
-	state = {
-		searchTerm: "",
-		redirect: ""
-	}
+	return (
+		<Navbar collapseOnSelect expand="md" bg="dark" variant="dark" sticky="top">
+			<LinkContainer exact to="/">
+				<NavLink active={false}>
+					{/* <Navbar.Brand> */}
+					<NavItem>Home</NavItem>
+					{/* </Navbar.Brand> */}
+				</NavLink>
+			</LinkContainer>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="mr-auto">
+					<LinkContainer exact  to="/MyCookbook">
+						<NavLink active={false}><NavItem>My Cookbook</NavItem></NavLink>
+					</LinkContainer>
+					<LinkContainer exact to="/MyFridge">
+						<NavLink active={false}><NavItem>My Fridge</NavItem></NavLink>
+					</LinkContainer>
+					<LinkContainer exact to="/MyShoppingList">
+						<NavLink active={false}><NavItem>My Shopping List</NavItem></NavLink>
+					</LinkContainer>
+				</Nav>
+				<Nav className="ml-auto">
+					<LinkContainer exact to="/Login">
+						<NavLink active={false}><NavItem>Login</NavItem></NavLink>
+					</LinkContainer>
+					<LinkContainer exact to="/SignUp">
+						<NavLink active={false}><NavItem>Sign Up</NavItem></NavLink>
+					</LinkContainer>
+				</Nav>
 
-	handleOnChange = (event) => {
-		event.preventDefault();
-		this.setState({
-			searchTerm: event.target.value
-		})
-	}
-
-	handleSubmit = (event) => {
-		event.preventDefault();
-		// let query = {
-		// 	term: this.state.searchTerm,
-		// 	from: 0,
-		// 	to: 5
-		// }
-		// recipeSearch(query)
-		// .then(res => {
-		// 	let recipes = res.hits.map(hit => Object.assign({}, hit.recipe, {starred: false}))
-		// 	this.props.addRecipeResults(recipes)
-		// 	console.log(JSON.stringify(recipes))
-		// })
-		this.setState({searchTerm: ""});
-		this.props.history.push("/Search")
-	}
-
-	render() {
-		return (
-			<Navbar collapseOnSelect expand="md" bg="dark" variant="dark" sticky="top">
-				<LinkContainer exact to="/">
-					<NavLink active={false}>
-						{/* <Navbar.Brand> */}
-						<NavItem>Home</NavItem>
-						{/* </Navbar.Brand> */}
-					</NavLink>
-				</LinkContainer>
-				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
-					<Nav className="mr-auto">
-						<LinkContainer exact  to="/MyCookbook">
-							<NavLink active={false}><NavItem>My Cookbook</NavItem></NavLink>
-						</LinkContainer>
-						<LinkContainer exact to="/MyFridge">
-							<NavLink active={false}><NavItem>My Fridge</NavItem></NavLink>
-						</LinkContainer>
-						<LinkContainer exact to="/MyShoppingList">
-							<NavLink active={false}><NavItem>My Shopping List</NavItem></NavLink>
-						</LinkContainer>
-					</Nav>
-					<Nav className="ml-auto">
-						<LinkContainer exact to="/Login">
-							<NavLink active={false}><NavItem>Login</NavItem></NavLink>
-						</LinkContainer>
-						<LinkContainer exact to="/SignUp">
-							<NavLink active={false}><NavItem>Sign Up</NavItem></NavLink>
-						</LinkContainer>
-					</Nav>
-
-					{/* <Form inline onSubmit={this.handleSubmit}>
-					<Form.Group controlId="recipeSearchForm">
-						<FormControl onChange={this.handleOnChange} value={this.state.searchTerm} type="text" placeholder="Find Recipes ..." className="mr-sm-3"/>
-					</Form.Group>
-					<Form.Group controlId="recipeSearchFormSubmitButton">
-						<Button type="submit" variant="outline-success">Search</Button>
-					</Form.Group>
-					</Form> */}
-				</Navbar.Collapse>
-			</Navbar>
-		)
-	}
+				{/* <Form inline onSubmit={this.handleSubmit}>
+				<Form.Group controlId="recipeSearchForm">
+					<FormControl onChange={this.handleOnChange} value={this.state.searchTerm} type="text" placeholder="Find Recipes ..." className="mr-sm-3"/>
+				</Form.Group>
+				<Form.Group controlId="recipeSearchFormSubmitButton">
+					<Button type="submit" variant="outline-success">Search</Button>
+				</Form.Group>
+				</Form> */}
+			</Navbar.Collapse>
+		</Navbar>
+	)
 }
 
 const mapStateToProps = state => {
