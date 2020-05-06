ff6c651d5de0d759e3acf82dc1c0f0a2f8243463
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 07:13:32 +0100

    sample fetch requests and comments

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -38,3 +38,103 @@ const App = () => {
 }
 
 export default App;
+
+// SAMPLE FETCH TO SIGNUP
+
+// fetch('http://localhost:4000/api/v1/users', {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 		Accept: 'application/json'
+// 	},
+// 	body: JSON.stringify({
+// 		user: {
+// 			username: "niki",
+// 			password: "potato"
+// 		}
+// 	})
+// })
+// 	.then(r => r.json())
+// 	.then(console.log)
+
+// SAMPLE FETCH TO LOGIN
+
+// fetch('http://localhost:4000/api/v1/login', {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 		Accept: 'application/json'
+// 	},
+// 	body: JSON.stringify({
+// 		user: {
+// 			username: "niki",
+// 			password: "potato"
+// 		}
+// 	})
+// })
+// 	.then(r => r.json())
+// 	.then(res => {data = JSON.parse(res.user_data);console.log(res)})
+
+// RETURNS USER_INGREDIENTS_DATA
+
+
+// SAMPLE FETCH TO ALL RECIPES BY A USER
+
+// fetch('http://localhost:4000/api/v1/chefs/niki')
+// .then(r => r.json()).then(console.log)
+
+// SAMPLE CREATE A RECIPE
+
+// fetch('http://localhost:4000/recipes/new', {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 		Accept: 'application/json',
+// 		Authorization: `Bearer ${token}`
+// 	},
+// 	body: JSON.stringify({
+// 		recipe: {
+// 			uuid: "ba41038-d4a7-77c0-8e30-cb6dbfc064",
+// 			title: "New UGHHHHH testis",
+// 			description: "whatever descr",
+// 			cookingTime: 12,
+// 			prepTime: 10,
+// 			servingCount: 2,
+// 			imageLink: "https://www.thespruceeats.com/thmb/GknNSOSoICF6LsuXPN0PipSTAC8=/1500x844/smart/filters:no_upscale()/foolproof-traditional-pancake-recipe-435607-10_preview-5b0ee3358023b90036179861.jpeg",
+// 			instructions: "do the first thing|||do the second thing|||do the last thing",
+// 			ingredients: [{
+// 					uuid: 'aa1da3-f1bb-bb82-ca11-36a5e20e2452',
+// 					weight: '200'
+// 				},
+// 				{
+// 					uuid: '38a436-0da-bce4-a7a2-3751068bc',
+// 					weight: '150'
+// 				},
+// 				{
+// 					uuid: 'a2f77-cb3-8c1-b806-8cde7d11a8e',
+// 					weight: '100'
+// 				}]
+// 		}
+// 	})
+// })
+// 	.then(r => r.json())
+// 	.then(console.log)
+
+// FETCH UPDATE USER INGREDIENTS
+
+// fetch('http://localhost:4000/api/v1/ingredients/update', {
+// 	method: 'POST',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 		Accept: 'application/json',
+// 		Authorization: `Bearer ${token}`
+// 	},
+// 	body: JSON.stringify({
+// 		 user: {
+// 				ingredients: newIng
+// 		 }
+// 	})
+// })
+// 	.then(r => r.json())
+// 	.then(console.log)
+
--- src/Containers/RecipePage.js
+++ src/Containers/RecipePage.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import PageHeader from '../Components/PageHeader'
+import { useHistory } from 'react-router'
 
 const RecipePage = (props) => {
+	// useHistory FOR THE ADDRESS I GUESS
+	// useEffect(() => {FETCH THE SPECIFIC PAGE USING THE UUID IN THE ADDRESS}, [])
 	return (
 		<div>
 			<PageHeader title={"12 Pancakes with Nutella and Berries"}/>
