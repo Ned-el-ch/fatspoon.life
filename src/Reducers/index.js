cfd4eee3f02319a25ec510fcc505c4ee8dcc147e
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 07:40:16 +0100

    Add user.js to combined reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import user from './user';
 import recipes from './recipes';
 import ingredients from './ingredients';
 import search from './search';
 
 export default combineReducers({
+	user,
 	recipes,
 	ingredients,
 	search
