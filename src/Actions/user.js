a5d5b65fdb2f7e65c3e486295fe5cdb666234aa4
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 07:56:03 +0100

    Add userLoginFetch action

--- src/Actions/user.js
+++ src/Actions/user.js
@@ -20,6 +20,28 @@ export const userPostFetch = user => {
 	}
 }
 
+export const userLoginFetch = user => {
+	return dispatch => {
+		return fetch("http://localhost:3000/api/v1/login", {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+			body: JSON.stringify({user})
+		})
+			.then(res => res.json())
+			.then(data => {
+				if (data.jwt) {
+					localStorage.setItem("token", data.jwt)
+					dispatch(loginUser(data.user_data))
+				} else {
+					//ADD LOGIC TO HANDLE WRONG STUFF
+				}
+			})
+	}
+}
+
 const loginUser = userData => ({
 		type: 'LOGIN_USER',
 		userData
