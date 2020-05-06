cae9f3de2625c3b73d41ce26c3e2516c2770c8ec
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 08:15:59 +0100

    Add getProfileFetch to auto-login if token is present

--- src/Actions/user.js
+++ src/Actions/user.js
@@ -10,9 +10,9 @@ export const userPostFetch = user => {
 		})
 			.then(res => res.json())
 			.then(data => {
-				if (data.jwt) {
+				if (data.token) {
 					localStorage.setItem("token", data.token)
-					dispatch(loginUser(data.user_data))
+					dispatch(loginUser(JSON.parse(data.user_data)))
 				} else {
 					// ADD LOGIC TO HANDLE WRONG STUFF
 				}
@@ -32,13 +32,38 @@ export const userLoginFetch = user => {
 		})
 			.then(res => res.json())
 			.then(data => {
-				if (data.jwt) {
+				if (data.token) {
 					localStorage.setItem("token", data.token)
-					dispatch(loginUser(data.user_data))
+					dispatch(loginUser(JSON.parse(data.user_data)))
 				} else {
 					//ADD LOGIC TO HANDLE WRONG STUFF
 				}
 			})
+	}
+}
+
+export const getProfileFetch = () => {
+	return dispatch => {
+		const token = localStorage.token;
+		if (token) {
+			return fetch("http://localhost:3000/api/v1/profile", {
+				method: "GET",
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+					'Authorization': `Bearer ${token}`
+				}
+			})
+				.then(res => res.json())
+				.then(data => {
+					if (data.message) {
+						// IF AN ERROR MESSAGE IS RECEIVED, CLEAR TOKEN
+						localStorage.removeItem("token")
+					} else {
+						dispatch(loginUser(data.user))
+					}
+				})
+		}
 	}
 }
 
