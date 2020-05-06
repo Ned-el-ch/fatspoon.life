7f70dcb44c78da4a3460e5c91ed7a34b593ee1ba
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 07:38:25 +0100

    Update LoginPage.js

--- src/Containers/LoginPage.js
+++ src/Containers/LoginPage.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const LoginPage = () => {
 	return (
-		<Form>
-			<Form.Group controlId="formUsername">
-				<Form.Label>Username</Form.Label>
-				<Form.Control type="username" placeholder="Enter Username"/>
-				<Form.Text className="text-muted">
-					This is what you'll be referred to
-				</Form.Text>
-			</Form.Group>
+		<Row>
+		<Col xs={12} sm={12} md={{ span: 10, offset: 1}} lg={{ span: 10, offset: 1}} className="rf-remove-margin">
+			<Form>
+				<Form.Group controlId="formUsername">
+					<Form.Label>Username</Form.Label>
+					<Form.Control type="username" placeholder="Enter Username"/>
+					<Form.Text className="text-muted">
+						This is what you'll be referred to
+					</Form.Text>
+				</Form.Group>
 
-			<Form.Group controlId="formPassword">
-				<Form.Label>Password</Form.Label>
-				<Form.Control type="password" placeholder="Password"/>
-			</Form.Group>
-			<Button variant="primary" type="submit">
-				Submit
-			</Button>
-		</Form>
+				<Form.Group controlId="formPassword">
+					<Form.Label>Password</Form.Label>
+					<Form.Control type="password" placeholder="Password"/>
+				</Form.Group>
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</Col>
+		</Row>
 	)
 }
 
