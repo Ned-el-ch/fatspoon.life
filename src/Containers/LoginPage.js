47528aa4c58cef24a924094d1d0d5119adee7b53
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 07:20:51 +0100

    Update LoginPage.js

--- src/Containers/LoginPage.js
+++ src/Containers/LoginPage.js
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import Form from 'react-bootstrap/Form'
 const LoginPage = () => {
 	return (
-		<div>
-			
-		</div>
+		<Form>
+			<Form.Group controlId="formUsername">
+				<Form.Label>Username</Form.Label>
+				<Form.Control type="username" placeholder="Enter Username"/>
+				<Form.Text className="text-muted">
+					This is what you'll be referred to
+				</Form.Text>
+			</Form.Group>
+
+			<Form.Group controlId="formPassword">
+				<Form.Label>Password</Form.Label>
+				<Form.Control type="password" placeholder="Password"/>
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Submit
+			</Button>
+		</Form>
 	)
 }
 
