520566b6e070a1ad2f3590b0f1b8d4b5680e44bd
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-06 08:49:33 +0100

    Update store.js to add thunkMiddleware

--- src/store.js
+++ src/store.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import rootReducer from './Reducers/index';
 
 const store = createStore(
-	rootReducer, 
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	rootReducer,
+	compose(
+		applyMiddleware(
+			thunkMiddleware
+		),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 );
 
 export default store;
