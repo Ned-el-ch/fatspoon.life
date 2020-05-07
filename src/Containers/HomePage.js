7c16a990ff9a5ecfdcb9665c082594d8d997935e
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-07 08:43:46 +0100

    Add app description

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -6,8 +6,8 @@ const HomePage = () => {
 	return (
 		<div className="meal-planner-container">
 			<PageHeader title="Welcome to Fatspoon!"/>
-
-			<Link to="/MyCookbook">Add some recipes first!</Link>
+			<span className="hp-app-description">Fatspoon makes it easy for you to cook at home more often by helping you sort out the ingredients you have on hand in <Link to="/MyFridge">My Fridge</Link>, find recipes you can make with them, as well as create and store your own recipes in <Link to="/MyCookbook">My Cookbook</Link>! Oh, and if you're missing an item just add it to my <Link to="/MyShoppingList">Shopping List</Link>!</span>
+			{/* <Link to="/MyCookbook">Add some recipes first!</Link> */}
 		</div>
 	)
 }
