8ac4e880cceb36498ca4beea820ec9b7eac80d3f
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-07 08:46:51 +0100

    get started info

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -6,8 +6,12 @@ const HomePage = () => {
 	return (
 		<div className="meal-planner-container">
 			<PageHeader title="Welcome to Fatspoon!"/>
-			<span className="hp-app-description">Fatspoon makes it easy for you to cook at home more often by helping you sort out the ingredients you have on hand in <Link to="/MyFridge">My Fridge</Link>, find recipes you can make with them, as well as create and store your own recipes in <Link to="/MyCookbook">My Cookbook</Link>! Oh, and if you're missing an item just add it to my <Link to="/MyShoppingList">Shopping List</Link>!</span>
-			{/* <Link to="/MyCookbook">Add some recipes first!</Link> */}
+			<div className="hp-app-description-container">
+				<span className="hp-app-description">Fatspoon makes it easy for you to cook at home more often by helping you sort out the ingredients you have on hand in <Link to="/MyFridge">My Fridge</Link>, find recipes you can make with them, as well as create and store your own recipes in <Link to="/MyCookbook">My Cookbook</Link>! Oh, and if you're missing an item just add it to my <Link to="/MyShoppingList">Shopping List</Link>!</span>
+			</div>
+			<div className="hp-app-get-started-container">
+			<span>Get started by <Link to="/Login">logging in</Link> or <Link to="/SignUp">signing up</Link>!</span>
+			</div>
 		</div>
 	)
 }
