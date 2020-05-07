35077b51fefceb858cf478394c0eeb570ec19517
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-07 16:10:05 +0100

    Styling changes

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 
 const HomePage = () => {
 	return (
-		<div className="meal-planner-container">
+		<div className="hp-container">
 			<PageHeader title="Welcome to Fatspoon!"/>
 			<div className="hp-app-description-container">
 				<span className="hp-app-description">Fatspoon makes it easy for you to cook at home more often by helping you sort out the ingredients you have on hand, find recipes you can make with them, as well as create and store your own recipes! Oh, and if you're missing an item just add it to the built-in shopping list!</span>
 			</div>
 			<div className="hp-app-get-started-container">
-			<span>Get started by <Link to="/Login">logging in</Link> or <Link to="/SignUp">signing up</Link>!</span>
+			<span className="hp-app-get-started">Get started by <Link to="/Login">logging in</Link> or <Link to="/SignUp">signing up</Link>!</span>
 			</div>
 		</div>
 	)
