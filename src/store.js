7705d69853081d2b5cf0dd16d0466ac492eee7e1
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-07 11:28:35 +0100

    fix blank page when missing redux devtools

--- src/store.js
+++ src/store.js
@@ -2,14 +2,14 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './Reducers/index';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunkMiddleware)
+);
 const store = createStore(
 	rootReducer,
-	compose(
-		applyMiddleware(
-			thunkMiddleware
-		),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	enhancer
 );
 
 export default store;
