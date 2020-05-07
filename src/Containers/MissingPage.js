b149d21973cccfba2117a60ac145f20110b553b2
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-07 12:14:55 +0100

    Create MissingPage.js for unmatched routes

---
+++ src/Containers/MissingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../Components/PageHeader'
+
+const MissingPage = () => {
+	return (
+		<div>
+			<PageHeader title={"This page is in another castle"}/>
+		</div>
+	)
+}
+
+export default MissingPage
