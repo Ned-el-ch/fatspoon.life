1296ea86d4d7e35a190da22798a68352d577fd69
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-07 21:38:48 +0100

    Update HomePage.js

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -2,16 +2,23 @@ import React from 'react'
 import PageHeader from '../Components/PageHeader.js';
 import { Link } from 'react-router-dom';
 
-const HomePage = () => {
+const HomePage = ({userIsLoggedIn}) => {
 	return (
 		<div className="hp-container">
 			<PageHeader title="Welcome to Fatspoon!"/>
 			<div className="hp-app-description-container">
 				<span className="hp-app-description">Fatspoon makes it easy for you to cook at home more often by helping you sort out the ingredients you have on hand, find recipes you can make with them, as well as create and store your own recipes! Oh, and if you're missing an item just add it to the built-in shopping list!</span>
 			</div>
+			{userIsLoggedIn
+			?
 			<div className="hp-app-get-started-container">
-			<span className="hp-app-get-started">Get started by <Link to="/Login">logging in</Link> or <Link to="/SignUp">signing up</Link>!</span>
+				<span className="hp-app-get-started">Add some ingredients to <Link to="/MyFridge">My Fridge</Link> or maybe add a recipe to <Link to="/Cookbook">My Cookbook</Link>!</span>
 			</div>
+			:
+			<div className="hp-app-get-started-container">
+				<span className="hp-app-get-started">Get started by <Link to="/Login">logging in</Link> or <Link to="/SignUp">signing up</Link>!</span>
+			</div>
+			}
 		</div>
 	)
 }
