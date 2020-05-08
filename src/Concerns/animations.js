2ae57cbe5869082531ab44e870e8248821b0a696
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-08 16:17:59 +0100

    import CSSRulePlugin

--- src/Concerns/animations.js
+++ src/Concerns/animations.js
@@ -1,37 +1,15 @@
-import { TimelineMax as Timeline, Power1 } from 'gsap';
+import { gsap, TimelineMax as Timeline, Power1 } from 'gsap';
+import { CSSRulePlugin } from "gsap/CSSRulePlugin";
+gsap.registerPlugin(CSSRulePlugin);
 
-// const getDefaultTimeline = (node, delay) => {
-// 	const timeline = new Timeline({ paused: true });
-// 	const content = node.querySelector('.content');
-// 	const contentInner = node.querySelector('.content--inner');
-
-// 	timeline
-// 		.from(node, 0.075, { display: 'none', autoAlpha: 0, delay, ease: Power1.easeIn })
-// 		.from(content, 0.075, { autoAlpha: 0, y: 25, ease: Power1.easeInOut })
-// 		.from(contentInner, 0.075, { autoAlpha: 0, delay: 0.15, ease: Power1.easeIn });
-
-// 	return timeline;
-// }
-
-const getHomeTimeline = (node, delay) => {
+export const play = (pathname, node, appears) => {
+	const delay = appears ? 0 : 0.05;
 	const timeline = new Timeline({ paused: true });
 	const texts = node.querySelectorAll('.content > div');
 
 	timeline
 		.from(node, 0.075, { display: 'none', autoAlpha: 0, delay })
 		.staggerFrom(texts, 0.25, { autoAlpha: 0, x: -25, ease: Power1.easeOut }, 0.125);
-
-	return timeline;
-}
-
-export const play = (pathname, node, appears) => {
-	const delay = appears ? 0 : 0.05;
-	let timeline = getHomeTimeline(node, delay);
-
-	// if (pathname === '/')
-	// 	timeline = getHomeTimeline(node, delay);
-	// else
-	// 	timeline = getDefaultTimeline(node, delay);
 
 	window
 		.loadPromise
