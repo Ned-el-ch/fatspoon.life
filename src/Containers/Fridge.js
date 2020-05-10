8f63a3abc08eb8e7d930d91791d39f6de183b4c1
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-10 09:16:15 +0100

    Add filter by name to Fridge.js

--- src/Containers/Fridge.js
+++ src/Containers/Fridge.js
@@ -4,6 +4,10 @@ import IngredientCard from '../Components/IngredientCard.js';
 import AnimatedSelect from '../Components/AnimatedSelect.js';
 import { connect } from 'react-redux'
 import { removeIngredient } from '../Actions/ingredients'
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import InputGroup from "react-bootstrap/InputGroup"
+import FormControl from "react-bootstrap/FormControl"
 
 const Fridge = ({ingredients, removeIngredient, increaseIngredient, decreaseIngredient}) => {
 
@@ -17,23 +21,35 @@ const Fridge = ({ingredients, removeIngredient, increaseIngredient, decreaseIngr
 				CHECK IF THE USER HAS ANY INGREDIENTS IN THEIR FRIDGE, IF NOT RENDER
 				"LOOKS LIKE YOU DON'T HAVE INGREDIENTS YET, WHAT WOULD YOU LIKE TO ADD?"
 				*/}
-				<AnimatedSelect/>
+				<Row>
+				<Col xs={12} sm={12} md={12} lg={12} className="rf-remove-margin">
+					<AnimatedSelect/>
+				</Col>
+				</Row>
 				<div className="fridge-ingredients-container">
-					<div className="fridge-ingredients-filter">
-					</div>
 					<div className="fridge-ingredients">
+					<Row>
+					<Col xs={12} sm={12} md={{span: 10, offset: 1}} lg={{span: 10, offset: 1}} className="rf-remove-margin">
 						<div className="fridge-ingredients-filter-container">
-							<span>Find by name: </span>
-							<input
-								type="text"
-								// name="filter"
+						<InputGroup className="mb-3">
+							<InputGroup.Prepend>
+								<InputGroup.Text id="inputGroup-sizing-lg">Find by</InputGroup.Text>
+							</InputGroup.Prepend>
+							<FormControl
+								placeholder="ingredient name"
+								aria-label="title"
+								aria-describedby="inputGroup-sizing-lg"
 								value={filter}
+								maxLength={25}
 								onChange={event => {
 									event.preventDefault()
 									setFilter(event.target.value)
 								}}
 							/>
+						</InputGroup>
 						</div>
+					</Col>
+					</Row>
 						{filter
 						?
 							ingredients.filter(e => e.name.toLowerCase().includes(filter.toLowerCase())).map(ingredient => {
