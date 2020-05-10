e2d3dd666fca3e5260ccfe72e606d4c120456a0b
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-10 19:34:36 +0100

    add fetchAddToMealPlan action

--- src/Actions/mealPlan.js
+++ src/Actions/mealPlan.js
@@ -17,13 +17,40 @@ export const removeFromMealPlan = recipeID => {
 	)
 }
 
-
-export const fetchAddToMealPlan = recipeID => {
-	addToMealPlan(recipeID)
+export const fetchAddToMealPlan = (recipe, date) => {
+	addToMealPlan(recipe, date)
 	let token = localStorage.token;
 	if (token) {
 		return dispatch => {
 			return fetch("https://calm-brook-68370.herokuapp.com/meal_planner/add", {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+					'Authorization': `Bearer ${token}`
+				},
+				body: JSON.stringify({recipe: {uuid: recipe.uuid}})
+			})
+			.then(res => res.json())
+			.then(data => {
+				if (data.error || data.message) {
+					console.log(data)
+					displatch(removeFromMealPlan(recipe.uuid))
+				} else {
+					debugger
+					dispatch(addToMealPlan(data.recipe, date))
+				}
+			})
+		}
+	}
+}
+
+export const fetchRemoveFromMealPlan = (recipe, date) => {
+	RemoveFromMealPlan(recipe.uuid)
+	let token = localStorage.token;
+	if (token) {
+		return dispatch => {
+			return fetch("https://calm-brook-68370.herokuapp.com/meal_planner/remove", {
 				method: "POST",
 				headers: {
 					'Content-Type': 'application/json',
@@ -36,10 +63,10 @@ export const fetchAddToMealPlan = recipeID => {
 			.then(data => {
 				if (data.error || data.message) {
 					console.log(data)
-					removeFromMealPlan(recipeID)
+					dispatch(addToMealPlan(data.recipe, date))
 				} else {
 					debugger
-					dispatch(addToMealPlan(data.recipe.uuid))
+					displatch(removeFromMealPlan(recipe.uuid))
 				}
 			})
 		}
