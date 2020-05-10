017ce361568c71ad79750e4f7e4fe7812d54cc43
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-10 18:26:55 +0100

    Update index.js

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux';
 import user from './user';
 import recipes from './recipes';
 import ingredients from './ingredients';
-import search from './search';
+// import search from './search';
+import mealPlan from './mealPlan';
 
 export default combineReducers({
 	user,
 	recipes,
 	ingredients,
-	search
+	mealPlan
 });
