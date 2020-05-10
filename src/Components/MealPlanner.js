d542a39403f7e68d86ad6c358a56d85b242c294f
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-10 11:01:10 +0100

    Update MealPlanner.js

--- src/Components/MealPlanner.js
+++ src/Components/MealPlanner.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import PageHeader from './PageHeader'
 
 const MealPlanner = () => {
 	return (
-		<div className="meal-planner">
-			
+		<div className="content meal-planner-container">
+			<PageHeader title="My Meal Planner"/>
+			<div className="content--inner">
+
+			</div>
 		</div>
 	)
 }
