282499798022c3c56604e3a45c0b0a1de7b4f6e0
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-11 17:48:52 +0100

    Update mealPlan.js

--- src/Reducers/mealPlan.js
+++ src/Reducers/mealPlan.js
@@ -5,17 +5,17 @@ export default (state = [], action) => {
 	switch (action.type) {
 
 		case "ADD_TO_MEAL_PLAN":
-			index = state.find(e => e.uuid === action.recipe.uuid)
+			index = state.find(e => e.uuid === action.uuid)
 			if (!!index) {
-				return [...state, {recipe: action.recipe, multiplier: action.multiplier}]
+				return [...state, {uuid: action.uuid, recipe: action.recipe, multiplier: action.multiplier, planned_date: action.planned_date}]
 			} else {
 				return state;
 			}
 
 		case "REMOVE_FROM_MEAL_PLAN":
-			index = state.find(e => e.uuid === action.recipeID)
+			index = state.find(e => e.uuid === action.uuid)
 			if (index) {
-				return state.filter(e => e.uuid !== action.recipeID)
+				return state.filter(e => e.uuid !== action.uuid)
 			} else {
 				return state;
 			}
