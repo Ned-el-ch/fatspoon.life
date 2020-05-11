5902d04ac94ff791c668f5c425e1fa6ed32bdac3
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-11 20:13:49 +0100

    add formatDate function

--- src/Concerns/generateExtra.js
+++ src/Concerns/generateExtra.js
@@ -18,3 +18,25 @@ export const createLabels = (recipe, ingredients) => {
 	};
 	return labels;
 }
+
+export const formatDate = date => {
+	let newDate = new Date(date.split("T")[0])
+	let daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	let monthNames = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	let day = daysOfTheWeek[newDate.getDay() - 1]
+	let month = monthNames[newDate.getMonth() - 1]
+	let number = newDate.getDate();
+	let year = newDate.getFullYear();
+	const th = (n) => {
+		if (n === 1 || n === 21 || n === 31) {
+			return `${n}st`
+		} else if (n === 2 || n === 22) {
+			return `${n}nd`
+		} else if (n === 3 || n === 23) {
+			return `${n}rd`
+		} else {
+			return `${n}th`
+		}
+	}
+	return `${day}, ${th(number)} ${month}, ${year}`
+}
