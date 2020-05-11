cd56832ed60d6e1a1ad30235e39e8c888eb099b4
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-11 18:58:47 +0100

    add MealCards to MealPlanner

--- src/Components/MealPlanner.js
+++ src/Components/MealPlanner.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import PageHeader from './PageHeader'
+import MealCard from '../Components/MealCard'
+import { connect } from 'react-redux'
+import { fetchRemoveFromMealPlan } from "../Actions/mealPlan"
+import { generateLink, createLabels } from '../Concerns/generateExtra'
 
-const MealPlanner = () => {
+const MealPlanner = ({ingredients, meals, fetchRemoveFromMealPlan}) => {
 	return (
 		<div className="content meal-planner-container">
 			<PageHeader title="My Meal Planner"/>
 			<div className="content--inner">
-
+				{meals.map(meal => {
+					return (
+						<MealCard
+							key={meal.id}
+							meal={meal}
+							recipe={meal.recipe}
+							labels={createLabels(meal.recipe, ingredients)}
+							link={generateLink(meal.recipe)}
+						/>
+					)
+				})
+				}
 			</div>
 		</div>
 	)
 }
 
-export default MealPlanner;
+const mapStateToProps = state => {
+	return (
+		{
+			meals: state.mealPlan,
+			ingredients: state.ingredients
+		}
+	)
+}
+
+export default connect(mapStateToProps, { fetchRemoveFromMealPlan })(MealPlanner);
