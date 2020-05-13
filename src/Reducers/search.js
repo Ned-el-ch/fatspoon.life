1d86a102d75fcea53dde39fc3eccad22a1dd8741
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-13 20:35:19 +0100

    Update search.js

--- src/Reducers/search.js
+++ src/Reducers/search.js
@@ -1,9 +1,5 @@
-import * as seedRecipes from "../Data/recipes.json"
-import * as allIngredients from "../Data/ingredients.json"
-
 let initialState = {
-	recipes: seedRecipes.default,
-	ingredients: allIngredients.default
+	recipes: []
 }
 
 export default (state = initialState, action) => {
@@ -11,10 +7,10 @@ export default (state = initialState, action) => {
 	switch (action.type) {
 
 		case "ADD_RECIPE_RESULTS":
-			return Object.assign({}, state, {recipes: action.recipeResults});
+			return {recipes: action.recipeResults};
 
 		case "CLEAR_RECIPE_RESULTS":
-			return [];
+			return {recipes: []};
 
 		default:
 			return state;
