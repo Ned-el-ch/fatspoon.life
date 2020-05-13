24bb2761062ea8bd45fea9e343de8da5c9633d86
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-13 20:35:31 +0100

    add search to store

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -2,12 +2,13 @@ import { combineReducers } from 'redux';
 import user from './user';
 import recipes from './recipes';
 import ingredients from './ingredients';
-// import search from './search';
+import search from './search';
 import mealPlan from './mealPlan';
 
 export default combineReducers({
 	user,
 	recipes,
 	ingredients,
-	mealPlan
+	mealPlan,
+	search
 });
