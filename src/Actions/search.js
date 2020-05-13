7c7bd7c9c2deb83b70855916c5fa5a34ed24af24
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-13 20:35:07 +0100

    style searchPage

--- src/Actions/search.js
+++ src/Actions/search.js
@@ -4,3 +4,25 @@ export const addRecipeResults = recipeResults => {
 		recipeResults
 	}
 }
+
+export const clearRecipeResults = recipeResults => {
+	return {
+		type: "CLEAR_RECIPE_RESULTS",
+		recipeResults
+	}
+}
+
+export const fetchSearchRecipes = (query) => {
+	return dispatch => {
+		return fetch(`https://calm-brook-68370.herokuapp.com/search/${query}`)
+			.then(res => res.json())
+			.then(data => {
+				if (data.error || data.message) {
+					console.log(data)
+				} else {
+					// debugger
+					dispatch(addRecipeResults(data))
+				}
+			})
+	}
+}
