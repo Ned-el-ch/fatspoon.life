0719c5f303c91ea0a0e4b9b31c4e0cb2ff58949d
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-15 11:34:49 +0100

    Add screenshot carousel

--- src/Containers/AboutPage.js
+++ src/Containers/AboutPage.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import PageHeader from '../Components/PageHeader'
 import { Link } from 'react-router-dom';
+import { ScreenshotCarousel } from '../Components/ScreenshotCarousel';
 
 const AboutPage = () => {
 	return (
 		<div className="content ap-container">
 			<PageHeader title={"Made by Niki Nedelchev"}/>
 			<div className="content--inner">
-			<div className="hp-app-get-started-container">
+				<div className="hp-app-get-started-container">
+					<span className="hp-app-get-started">Here are some screenshots!</span>
+				</div>
+				<ScreenshotCarousel/>
+				<div className="hp-app-get-started-container">
 					<span className="hp-app-get-started">Get started by adding your own recipes in <Link to="/MyCookbook">My Cookbook</Link> or <Link to="/Search">Search</Link> for some! When you view a recipe authored by someone else you can star it, and it'll be added to your cookbook!</span>
 				</div>
 				<div className="hp-app-get-started-container">
@@ -19,7 +24,9 @@ const AboutPage = () => {
 				<div className="hp-app-get-started-container">
 					<span className="hp-app-get-started">When you go shopping, just click DONE on each item from your shopping list to add it to <Link to="/MyFridge">your Fridge</Link>. If you click on an item you can edit the weight to match how much you've purchased!</span>
 				</div>
+				<div className="hp-app-get-started-container">
 				<span className="ap-contact">Find me on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Ned_el_ch">Twitter</a>, <a target="_blank" rel="noopener noreferrer" href="https://github.com/Ned-el-ch">GitHub</a>, and <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ned-el-ch/">LinkedIn</a></span>
+				</div>
 				<span className="ap-description">This is an SPA I built to practice using React, Redux, SCSS, and Ruby on Rails (as an API). Check out the <a target="_blank" rel="noopener noreferrer" href="https://github.com/Ned-el-ch/fatspoon.life">frontend repo</a> or the <a target="_blank" rel="noopener noreferrer" href="https://github.com/Ned-el-ch/fatspoon.life-backend">backend repo</a> if you want to know more.</span>
 				<span className="ap-packages">List of main things I used for the frontend</span>
 				<ul className="ap-ul">
