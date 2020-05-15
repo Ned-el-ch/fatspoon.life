5edd8c9ffd831d340cac8006599f6461d7b30bf9
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-15 09:08:09 +0100

    Update AboutPage.js

--- src/Containers/AboutPage.js
+++ src/Containers/AboutPage.js
@@ -7,11 +7,14 @@ const AboutPage = () => {
 		<div className="content ap-container">
 			<PageHeader title={"Made by Niki Nedelchev"}/>
 			<div className="content--inner">
-				<div className="hp-app-get-started-container">
-					<span className="hp-app-get-started">Get started by adding some recipes in <Link to="/MyCookbook">My Cookbook</Link> or <Link to="/Search">Search</Link> for some recipes!</span>
+			<div className="hp-app-get-started-container">
+					<span className="hp-app-get-started">Get started by adding your own recipes in <Link to="/MyCookbook">My Cookbook</Link> or <Link to="/Search">Search</Link> for some! When you view a recipe authored by someone else you can star it, and it'll be added to your cookbook!</span>
 				</div>
 				<div className="hp-app-get-started-container">
-					<span className="hp-app-get-started">When you view a recipe you can add it to your <Link to="/">Meal Planner</Link>, choosing the number of servings and date. Your <Link to="/MyShoppingList">Shopping List</Link> will automatically populate with the items you need for the current week!</span>
+					<span className="hp-app-get-started">Additionally, when viewing a recipe you can add it to your <Link to="/">Meal Planner</Link>, choosing the number of servings and date. If you've planned multiple recipes in a week, the meal planner will show you on which one you'll run out of a certain ingredient!</span>
+				</div>
+				<div className="hp-app-get-started-container">
+					<span className="hp-app-get-started">Your <Link to="/MyShoppingList">Shopping List</Link> will automatically populate with the items you need for the current week!</span>
 				</div>
 				<div className="hp-app-get-started-container">
 					<span className="hp-app-get-started">When you go shopping, just click DONE on each item from your shopping list to add it to <Link to="/MyFridge">your Fridge</Link>. If you click on an item you can edit the weight to match how much you've purchased!</span>
