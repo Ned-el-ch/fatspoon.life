a1f2c62307f59a5ad76b15af6d0bbdff24df3615
Niki <51001838+Ned-el-ch@users.noreply.github.com>
2020-05-18 08:44:10 +0100

    Update HomePage.js

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
 			<PageHeader title="Welcome to Fatspoon!"/>
 			<div className="content--inner">
 				<div className="hp-app-description-container">
-					<span className="hp-app-description">Fatspoon makes it easy for you to cook at home more often by helping you sort out the ingredients you have on hand, find recipes you can make with them, as well as create and store your own recipes! Oh, and if you're missing an item just add it to the built-in shopping list!</span>
+					<span className="hp-app-description">Fatspoon makes it easy for you to cook at home more often by helping you sort out the ingredients you have on hand, find recipes you can make with them, as well as create and store your own recipes! Oh, and if you're missing an item it'll be automatically added to the built-in shopping list!</span>
 				</div>
 				<div className="hp-app-get-started-container">
 					<span className="hp-app-get-started">Get started by <Link to="/Login">logging in</Link> or <Link to="/SignUp">signing up</Link>!</span>
